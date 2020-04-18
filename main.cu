d6951e8c49e92cc0379c83eb1ba2921cb499f2fc
sandersyen <sandersyen37@gmail.com>
2020-04-18 15:55:27 -0500

    first draft

---
+++ main.cu
@@ -0,0 +1,44 @@
+// UW ID: cchang253, syeh6
+// Name: Chun-Ming Chang, Shang-Yen Yeh
+
+#include <iostream>
+#include <cmath>
+#include "mmul.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    /*
+        need to decide how many arguments we need 
+    */
+    if (argc < 3)
+    {
+        cout << "Didn't give enough arguments while calling CLAHE editor!";
+    }
+    else
+    {
+        try
+        {
+            /*
+                parse arguments 
+            */
+
+            /*
+                call kernal functions 
+            */
+
+            /*
+                display result. 
+            */
+            
+        }
+        catch (...)
+        {
+            cout << "Invalid argument!";
+        }
+    }
+
+    cout << "\n";
+    return 0;
+}
